57a5d2ce31531df640bf968ba144f6c6de62f12a
Shunichi Ikegami <sike.tm@gmail.com>
2023-10-20 11:23:29 +0900

    fix: rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,14 +20,14 @@ RSpec.configure do |config|
 
   FileUtils.mkdir_p "log"
 
-  Stripe.api_key = ENV.fetch("VCR_STRIPE_WEBHOOK_API_KEY")
+  Stripe.api_key = ENV.fetch("VCR_STRIPE_WEBHOOK_API_KEY", 'sk_test_dummy')
 
   VCR.configure do |c|
     c.cassette_library_dir = "spec/vcr"
     c.hook_into :webmock
     c.allow_http_connections_when_no_cassette = true
     c.ignore_localhost = true
-    c.filter_sensitive_data("<STRIPE_API_KEY>") { ENV.fetch("VCR_STRIPE_WEBHOOK_API_KEY") }
+    c.filter_sensitive_data("<STRIPE_API_KEY>") { Stripe.api_key }
 
     c.default_cassette_options[:record] = ENV.fetch("VCR_RECORD", "once").to_sym
     c.default_cassette_options[:drop_unused_requests] = true
